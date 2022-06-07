d586e2af3796d7024dd0ad62e7eee962af591743
Emiliano Martinez <eming.mtz@gmail.com>
2022-06-06 20:40:06 -0500

    Initial Commit

---
+++ JustoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustoApp
+//
+//  Created by Emi Mtz on 06/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
